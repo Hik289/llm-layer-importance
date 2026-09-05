2e949caf9bb80113448d929a64539a24a2d87d94
Hik289 <songxinyuan@pku.edu.cn>
2026-09-05 08:14:41 -1000

    chore: remove verbose code comments

--- peft_pretraining/modeling_llama.py
+++ peft_pretraining/modeling_llama.py
@@ -805,16 +805,6 @@ class LlamaPreTrainedModel(PreTrainedModel):
     _no_split_modules = ["LlamaDecoderLayer"]
     _keys_to_ignore_on_load_unexpected = [r"decoder\.version"]
 
-    # def _init_weights(self, module):
-    #     std = self.config.initializer_range
-    #     if isinstance(module, nn.Linear):
-    #         module.weight.data.normal_(mean=0.0, std=std)
-    #         if module.bias is not None:
-    #             module.bias.data.zero_()
-    #     elif isinstance(module, nn.Embedding):
-    #         module.weight.data.normal_(mean=0.0, std=std)
-    #         if module.padding_idx is not None:
-    #             module.weight.data[module.padding_idx].zero_()
     def _init_weights(self, module):
         std = self.config.initializer_range
         num_layers = self.config.num_hidden_layers
